fe5e08619d6e44caa0361e75ef0825ebcba6df51
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-16 14:38:25 -0400

    add event listener for the login button

--- app/src/main/java/com/youssefgirgeis/loginscreen/MainActivity.java
+++ app/src/main/java/com/youssefgirgeis/loginscreen/MainActivity.java
@@ -2,12 +2,29 @@ package com.youssefgirgeis.loginscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(onClickLoginButton);
     }
+
+    private View.OnClickListener onClickLoginButton = new View  .OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            
+        }
+    };
 }
